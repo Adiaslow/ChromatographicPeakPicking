270f92aadfce52c2c94b9ca94bf0022f11ee08a3
Adam Murray <a.murray0413@gmail.com>
2024-11-25 16:54:05 -0800

    Update building_block.py

--- peak_picking/building_block.py
+++ peak_picking/building_block.py
@@ -6,3 +6,11 @@ class BuildingBlock:
     name: Optional[str] = None
     smiles: Optional[str] = None
     properties: Optional[Dict[str, Any]] = None
+
+    def __eq__(self, other):
+        if not isinstance(other, BuildingBlock):
+            return False
+        return self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name)
